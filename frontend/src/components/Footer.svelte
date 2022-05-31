4a5dae1c33029068bc0cfc4a1763160ac0575575
pedro-git-projects <pedro.generic.email@gmail.com>
2022-05-31 17:39:49 -0300

    footer

---
+++ frontend/src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+	let ano = new Date().getFullYear()
+</script>
+
+<main>
+	<footer class="d-flex align-items-center justify-content-center mt-5">
+		Copyright &copy; { ano }
+	</footer>
+</main>
+
+<style>
+footer {
+	background: black;
+	color: white;
+	min-height: 3em;
+	font-size: 0.8em;
+}
+</style>
